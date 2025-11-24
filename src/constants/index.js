4cd3d9bff4a2a8e7741af5baf5e7415d666d2a46
tamim-devs <tamimh0727@gmail.com>
2025-11-24 22:55:15 +0600

    implement welocme section

--- src/constants/index.js
+++ src/constants/index.js
@@ -20,4 +20,8 @@ const navLinks = [
     },
   ]
 
-  export { navLinks , navIcons };
+   const FONT_WEIGHT = {
+      subtitle: { min: 100, max:400, default: 100 },
+      title: { min: 400, max: 900, default: 400 },
+    }
+  export { navLinks , navIcons, FONT_WEIGHT };
