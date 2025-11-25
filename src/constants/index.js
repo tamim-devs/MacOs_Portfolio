092d0e262925faecb5da68d3cce705e7bffcf4d9
tamim-devs <tamimh0727@gmail.com>
2025-11-25 10:40:31 +0600

    Implement docs presentation

--- src/constants/index.js
+++ src/constants/index.js
@@ -24,4 +24,43 @@ const navLinks = [
       subtitle: { min: 100, max:400, default: 100 },
       title: { min: 400, max: 900, default: 400 },
     }
-  export { navLinks , navIcons, FONT_WEIGHT };
+
+  const dockApps = [
+    {
+      id: "finder",
+      name: "Portfolio",
+      icon: "../../public/images/finder.png",
+      canOpen: true
+    },
+    {
+      id: "safari",
+      name: "Articles",
+      icon: "../../public/images/safari.png",
+      canOpen: true
+    },
+    {
+      id: "photos",
+      name: "Gallery",
+      icon: "../../public/images/photos.png",
+      canOpen: false
+    },
+    {
+      id: "contact",
+      name: "Contact",
+      icon: "../../public/images/contact.png",
+      canOpen: true
+    },
+    {
+      id: 'terminal',
+      name: "Skills",
+      icon: "../../public/images/terminal.png",
+      canOpen: false
+    },
+    {
+      id: 'trash',
+      name: "Archive",
+      icon: "../../public/images/trash.png",
+      canOpen: false
+    }
+  ]
+  export { navLinks , navIcons, FONT_WEIGHT, dockApps };
